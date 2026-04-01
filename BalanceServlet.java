2affc83822e96afbeba573e45fdcb2704016aad5
Archana713117 <btsachu01@gmail.com>
2026-04-01 22:08:00 +0530

    Update BalanceServlet.java

--- BalanceServlet.java
+++ BalanceServlet.java
@@ -25,7 +25,7 @@ public class BalanceServlet extends HttpServlet {
             Connection conn = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/atmproject",
                 "root",
-                "Archana@07"
+                "your_password"
             );
 
             String query = "SELECT * FROM bank WHERE card=?";
